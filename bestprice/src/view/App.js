6b7a3d3dd75d719628df20d4f6970dbc1aa55c65
oleg <emerselling@gmail.com>
2019-05-23 14:08:37 +0300

    upd1

--- bestprice/src/view/App.js
+++ bestprice/src/view/App.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React, { useState, useEffect} from 'react';
->>>>>>> master
 import './App.css';
 import Header from './Header/Header';
-import Search from './Search/Search';
 import Nav from './Nav/Nav';
 import Category from './Category/Category';
 
--- bestprice/src/view/Nav/Nav.js
+++ bestprice/src/view/Nav/Nav.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:bestprice/src/view/Nav/Nav.js
 import React from 'react';
 import './Nav.css';
 
@@ -7,21 +6,9 @@ class Nav extends React.Component {
     return (
       <div className='navBar'>
       <h1>LOGO</h1><h1>Button</h1>
-=======
-import React, { useState, useEffect } from 'react';
-import './Ali.css';
-import example from '../../../../images/aliExample.png'
-
-function Ali(){
-    const [product, SetProduct] = useState('');
-
-    return (
-      <div className='wrapper'>
-        <img className='example'  src={example} alt="fireSpot" />
-        <div><img className='image' src={example} alt="fireSpot" /></div>
->>>>>>> master:bestprice/src/view/Middle/Shops/Aliexpress/Ali.js
       </div>
     );
+}
 }
 
 export default Nav;
