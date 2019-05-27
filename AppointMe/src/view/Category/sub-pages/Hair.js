b07dd05d1d22da762c187a31fe31a21f639764d3
Unknown <emerselling@gmail.com>
2019-05-27 19:40:47 +0300

    states start

--- AppointMe/src/view/Category/sub-pages/Hair.js
+++ AppointMe/src/view/Category/sub-pages/Hair.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from '../../Search/Search'
 import Sort from '../../Sort/Sort';
 
 function Hair() {
+    const [data, setData] = useState({title:'', info:''})
+    function test(){
+        setData({title:'Shop', info:'its a place with windows'})
+    }
+    
+
+
     return (
         <div>
             <Search/>
             <Sort/>
+            <button style={{height:'100px'}} onClick={test}>TEST</button>
+            <div>{data.title}{data.info}</div>
         </div>
     );
 }
