1f02b9a8256e19048f53a5ce597c3867d47732aa
oleg <emerselling@gmail.com>
2019-05-31 04:26:01 +0300

    5/31 register page 50%

--- AppointMe/src/view/Register/Register.js
+++ AppointMe/src/view/Register/Register.js
@@ -6,8 +6,13 @@ import './../Register/Register.css'
 
 function Register() {
     return (
-      <div className='test'>
-hello there mate
+      <div className='registerWrapper'>
+        <div className='regTitle'>Create a AppointMe Account</div>
+        <div className='inputPos'>Username<input className='inputSelf' id="name" required type='text'></input></div>
+        <div className='inputPos'>Email<input className='inputSelf' type='email'></input></div>
+        <div className='inputPos'>Password<input className='inputSelf' type='password'></input></div>
+        <div className='inputPos'>Confirm Password<input className='inputSelf' type='password'></input></div>
+        <div className='inputPos'><input className='inputSelf' type='submit' value='register'></input></div>
       </div>
     );
 }
